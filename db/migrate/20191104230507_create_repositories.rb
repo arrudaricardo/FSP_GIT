f835879f8b64a05838ad2d48dc3577300c98c54c
Ricardo de Arruda <settiricardo@gmail.com>
2019-11-04 15:20:45 -0800

    Generate repository model

---
+++ db/migrate/20191104230507_create_repositories.rb
@@ -0,0 +1,15 @@
+class CreateRepositories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :repositories do |t|
+      t.string :name, null: false
+      t.integer :owner_id, null: false
+
+      t.timestamps
+    end
+
+    add_index:repositories, :name
+    add_index:repositories, :owner_id 
+    add_index:repositories, [:name, :owner_id], unique: true
+
+  end
+end
