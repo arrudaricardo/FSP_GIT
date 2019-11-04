2ac099cb1f3ba1973f82b8c9bca48a82223f70dc
Ricardo de Arruda <settiricardo@gmail.com>
2019-11-04 15:03:13 -0800

    Create users migration file

---
+++ db/migrate/20191104225408_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :username, null: false
+      t.string :avatar_image
+      t.string :password_digest, null:false
+
+      t.timestamps
+
+    end
+
+    add_index users: :email, unique: true
+    add_index users: :username, unique: true
+    add_index :session_token, unique: true
+
+  end
+end
